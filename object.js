2cff854573fe4a078d7dd7fba04e0303c028ad9a
Krishna <krishna270402@gmail.com>
2024-07-04 12:58:57 +0530

    Basic

---
+++ object.js
@@ -0,0 +1,11 @@
+let person ={
+    name:'krish',
+    age:21,
+    sex:'male',
+    department:'Ece'
+}
+
+let fire = 'kumar';
+//console.log(fire);
+//console.log(person.age);
+console.log(person, fire);
---
+++ passfail.js
@@ -0,0 +1,6 @@
+const passMark = 50;
+let scoredMark = 40;
+if (passMark < scoredMark)
+  console.log("pass");
+else
+  console.log("Fail");
