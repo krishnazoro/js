7e4329f7a7d368b07f998066007d374217e5e524
krishnazoro <147299012+krishnazoro@users.noreply.github.com>
2024-07-10 13:00:09 +0530

    for loop

--- controlFlow/forloop.js
+++ controlFlow/forloop.js
@@ -1,10 +1,24 @@
+   for(let i = 0; i <= 10; i++ ){
+  if(i % 2 ===0)
+       console.log(i);
+
+  }
+
+  for (let i =10; i >= 1; i--){
+      if(i % 2 !==0)
+          console.log(i);
+  }
+
+
+
  for(let i = 0; i <= 10; i++ ){
- if(i % 2 !==0)
-     console.log(i);
-
-}
-
- for (let i =10; i >= 1; i--){
-     if(i % 2 !==0)
+     if(i % 2 ===0)
          console.log(i);
- }
+  
+   }
+
+   for(let i = 0; i <= 10; i++ ){
+    
+        console.log(i);
+  
+   }
