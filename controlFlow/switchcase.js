78f3ee35f9e7d8573bbf75f0b44b032f79add8fd
krishnazoro <147299012+krishnazoro@users.noreply.github.com>
2024-07-11 10:20:10 +0530

    code for switch case

---
+++ controlFlow/switchcase.js
@@ -0,0 +1,13 @@
+let role =guest;
+switch(role){
+    case 'guest':
+    console.log('guest user');
+    break;
+
+    case 'friend':
+     console.log('friend user');
+     break;
+
+     default:
+     console,log('unknown user');
+}
