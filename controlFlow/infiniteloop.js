c97b9a78bbe08465e9b8e1b73a9341e8917b7563
krishnazoro <147299012+krishnazoro@users.noreply.github.com>
2024-08-14 19:44:43 +0530

    infiniteloop

--- controlFlow/infiniteloop.js
+++ controlFlow/infiniteloop.js
@@ -1,15 +1,15 @@
-// // Don't execute this program
-// let i = 0;
-// while (i < 5){
-//     console.log(i);
+// Don't execute this program
+let i = 0;
+while (i < 5){
+    console.log(i);
     
-// }
+}
 
-// let i = 0;
-// do{
-//     console.log(i);
-// }
-// while (i < 5);
+let i = 0;
+do{
+    console.log(i);
+}
+while (i < 5);
 
 
 for(let i = 0;true;i++) {
