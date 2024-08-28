63eecaebe18924b8e6b410315780da6f9b731851
krishnazoro <147299012+krishnazoro@users.noreply.github.com>
2024-08-28 16:01:45 +0530

    learned abou object

--- exercises/ascending.js
+++ exercises/ascending.js
@@ -1,12 +1,25 @@
- let number = [1,6,,3,7,10,20,19];
- number.sort();
- console.log(number)
+//  let number = [1,6,,3,7,10,20,19];
+//  number.sort();
+//  console.log(number)
 
- let number = [1, 6, 3, 7, 10, 20, 19];
- number.reverse()
-  console.log(number)
+//  let number = [1, 6, 3, 7, 10, 20, 19];
+//  number.reverse()
+//   console.log(number)
 
-let animal = ['dog','elephant','zebra','duck','ant','cat'];
-animal.sort();
-console.log(animal);
+// let animal = ['dog','elephant','zebra','duck','ant','cat'];
+// animal.sort();
+// console.log(animal);
 
+let numbers =[34,17,45,23,67,76];
+console.log("orginal numbers",numbers);
+for(i = 0 ;i<=numbers.length;i++){
+    for(j = i+1;j<numbers.length;j++){
+       if( numbers[i]>numbers[j]){
+            let temp = numbers[j];
+            numbers[j]=numbers[i]
+            numbers[i] = temp;
+       }
+    }
+    
+}
+console.log(numbers)
--- exercises/object.js
+++ exercises/object.js
@@ -5,4 +5,9 @@ let bookInfo={
     language:'English'
 
 }
+bookInfo.jungle = "monkey";
+// TO add a new properities
 console.log(     bookInfo.author       );
+console.log(bookInfo);
+console.log("Hi i am", bookInfo.author,"and my mother tongue is",bookInfo.language)
+console.log(`hi i am ${bookInfo.author} and my mother tongue is${bookInfo.language}`)
